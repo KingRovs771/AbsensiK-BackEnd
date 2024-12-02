3bc04b3d926424442b57b042fcef82722bc73bda
Rizky Budiarto <53458876+KingRovs771@users.noreply.github.com>
2024-12-02 21:28:23 +0700

    Done Service TipePotongan

---
+++ internal/delivery/http/TipePotonganHandler.go
@@ -0,0 +1,39 @@
+package http
+
+type TipePotonganHandler struct{
+	TipePotonganService *services.TipePotonganService
+}
+
+func NewTipePotonganHandler(tipePotonganService *services.TipePotonganService) *TipePotonganHandler{
+	return &TipePotonganHandler{TipePotonganService : tipePotonganService}
+}
+
+func (h *TipePotonganHandler) GetAllTipePotongan(w http.ResponseWriter, r *http.Request){
+	response := h.TipePotonganService.GetAllTipePotongan()
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *TipePotonganHandler) CreateTipePotongan(w http.ResponseWriter, r *http.Request){
+	var TipePotongan models.Ak_TipePotongan
+	if err := json.NewDecoder(r.Body).Decode(&TipePotongan);err !=nil{
+		log.Println("Invalid Payload Request")
+		http.Error(w, "Invalid Payload Request", http.StatusBadRequest)
+	}
+
+	response := h.TipePotonganService.CreateTipePotongan(&TipePotongan)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func (h *TipePotonganHandler) GetTipePotonganById(w http.ResponseWriter, r *http.Request){
+
+}
+
+func (h *TipePotonganHandler) UpdateTipePotongan(w http.ResponseWriter, r *http.Request){
+
+}
+
+func (h *TipePotonganHandler) DeleteTipePotongan(w http.ResponseWriter, r *http.Request){
+
+}
--- internal/domain/services/tipepotongan_service.go
+++ internal/domain/services/tipepotongan_service.go
@@ -9,3 +9,100 @@ type TipePotonganService struct {
 func NewTipePotonganService(tipePotonganRepo *repository.TipePotonganRepository) *TipePotonganService {
 	return &TipePotonganService{TipePotonganRepo: tipePotonganRepo}
 }
+
+func (s *TipePotonganService) GetAllTipePotongan() map[string]interface{}{
+	TipePotongan, err := s.TipePotonganRepository.GetAllTipePotongan()
+
+	if err !=nil{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Data Tidak Ditemukan",
+			"Error" : err.Error(),
+		}
+	}
+
+	if len(TipePotongan) == 0{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Belum Ada Data Tipe Potongan",
+		}
+	}
+
+	return map[string]interface{}{
+		"Status" : "Success",
+		"Message" : "Data Berhasil Ditemukan"
+		"Tipe Potongan" : TipePotongan,
+	}
+}
+
+func (s *TipePotonganService) CreateTipePotongan(tipePotongan *models.Ak_TipePotongan)map[string]interface{}{
+	if err := s.TipePotonganRepository.CreateTipePotongan(tipePotongan); err != nil{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Gagal Menginput Data Tipe Potongan",
+			"Error" : err.Error(),
+		}
+	}
+
+	if tipePotongan.NamaPotongan == "" || tipePotongan.NilaiPotongan == 0{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Tolong Lengkapi Data Potongan",
+		}
+	}
+
+	return map[string]interface{}{
+		"Status" : "Success",
+		"Message" : "Data Berhasil Di Inputkan",
+		"Tipe Potongan" : tipePotongan
+	}
+}
+
+func (s *TipePotonganService) GetTipePotonganById(TipePotonganId int64) map[string]interface{}{
+	tipePotongan, err := s.TipePotonganRepository.GetTipePotonganById(TipePotonganId)
+
+	if err !=nil{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Data Tidak Ditemukan, silakan check lagi",
+			"Error" : err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"Status" : "Success",
+		"Message" : "Data Berhasil Ditemukan",
+		"Tipe Potongan" : tipePotongan
+	}
+}
+
+func (s *TipePotonganService) UpdateTipePotongan(TipePotongan *models.Ak_TipePotongan)map[string]interface{}{
+	if err := s.TipePotonganRepository.UpdateTipePotongan(TipePotongan); err !=nil{
+		return map[string]interface{}{
+			"Status" : "Error",
+			"Message" : "Data Tipe Potongan Tidak Berhasil Di update",
+			"Error" : err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"Status" : "Success",
+		"Message" : "Data Berhasil Di Update Silakan Check Data",
+		"Tipe Potongan" : TipePotongan,
+	}
+}
+
+func (s *TipePotonganService) DeleteTipePotongan(TipePotonganId int64) map[string]interface{}{
+	if err := s.TipePotonganRepository.DeleteTipePotongan(TipePotonganId); err !=nil{
+		return map[string]interface{}{
+			"Status" : "Error", 
+			"Message" : "Data Tidak Berhasil Di Update",
+			"Error" : err.Error(),
+		}
+	}
+
+	return map[string]interface{}{
+		"Status" : "Success",
+		"Message" : "Data Berhasil Di Hapus"
+	}
+}
