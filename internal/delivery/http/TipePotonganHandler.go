d0083754c14e2e7516d33e59565803e5fbea1778
rzbudi__ <rizkybudiarto890@gmail.com>
2024-12-03 07:17:41 +0700

    Done Tipe Potongan

--- internal/delivery/http/TipePotonganHandler.go
+++ internal/delivery/http/TipePotonganHandler.go
@@ -1,22 +1,31 @@
 package http
 
-type TipePotonganHandler struct{
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/KingRovs771/AbsensiK-BackEnd/internal/domain/models"
+	"github.com/KingRovs771/AbsensiK-BackEnd/internal/domain/services"
+)
+
+type TipePotonganHandler struct {
 	TipePotonganService *services.TipePotonganService
 }
 
-func NewTipePotonganHandler(tipePotonganService *services.TipePotonganService) *TipePotonganHandler{
-	return &TipePotonganHandler{TipePotonganService : tipePotonganService}
+func NewTipePotonganHandler(tipePotonganService *services.TipePotonganService) *TipePotonganHandler {
+	return &TipePotonganHandler{TipePotonganService: tipePotonganService}
 }
 
-func (h *TipePotonganHandler) GetAllTipePotongan(w http.ResponseWriter, r *http.Request){
+func (h *TipePotonganHandler) GetAllTipePotongan(w http.ResponseWriter, r *http.Request) {
 	response := h.TipePotonganService.GetAllTipePotongan()
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-func (h *TipePotonganHandler) CreateTipePotongan(w http.ResponseWriter, r *http.Request){
+func (h *TipePotonganHandler) CreateTipePotongan(w http.ResponseWriter, r *http.Request) {
 	var TipePotongan models.Ak_TipePotongan
-	if err := json.NewDecoder(r.Body).Decode(&TipePotongan);err !=nil{
+	if err := json.NewDecoder(r.Body).Decode(&TipePotongan); err != nil {
 		log.Println("Invalid Payload Request")
 		http.Error(w, "Invalid Payload Request", http.StatusBadRequest)
 	}
@@ -26,14 +35,14 @@ func (h *TipePotonganHandler) CreateTipePotongan(w http.ResponseWriter, r *http.
 	json.NewEncoder(w).Encode(response)
 }
 
-func (h *TipePotonganHandler) GetTipePotonganById(w http.ResponseWriter, r *http.Request){
+func (h *TipePotonganHandler) GetTipePotonganById(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *TipePotonganHandler) UpdateTipePotongan(w http.ResponseWriter, r *http.Request){
+func (h *TipePotonganHandler) UpdateTipePotongan(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *TipePotonganHandler) DeleteTipePotongan(w http.ResponseWriter, r *http.Request){
+func (h *TipePotonganHandler) DeleteTipePotongan(w http.ResponseWriter, r *http.Request) {
 
 }
