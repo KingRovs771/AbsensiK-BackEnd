dc0818a8017ab0baeea4c0cfc20e83207485a839
rzbudi__ <rizkybudiarto890@gmail.com>
2025-06-29 20:39:41 +0700

    Done Izin

--- internal/delivery/http/IzinHandler.go
+++ internal/delivery/http/IzinHandler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/KingRovs771/AbsensiK-BackEnd/internal/domain/models"
 	"github.com/KingRovs771/AbsensiK-BackEnd/internal/domain/services"
 	"github.com/gorilla/mux"
-	"golang.org/x/crypto/openpgp/errors"
 	"io"
 	"log"
 	"net/http"
@@ -169,20 +168,13 @@ func (h *IzinHandler) ApproveIzin(w http.ResponseWriter, r *http.Request) {
 
 func (h *IzinHandler) getUserFromRequest(r *http.Request) (*models.Ak_Users, error) {
 	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		http.Error("Could not fetch permit history", http.StatusInternalServerError)
-		return
-	}
+
 	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-	if tokenString == authHeader {
-		http.Error("Could not fetch permit history", http.StatusInternalServerError)
-		return
-	}
+
 	user, err := h.AuthService.GetUserFromToken(tokenString)
 	if err != nil {
-		http.Error("Could not fetch permit history", http.StatusInternalServerError)
-		return
 	}
+
 	return user, nil
 }
 func (h *IzinHandler) GetUserPermitHistory(w http.ResponseWriter, r *http.Request) {
