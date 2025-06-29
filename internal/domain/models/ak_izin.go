261c9a3d7feb552305d1a1d240e3fccfd9252563
rzbudi__ <rizkybudiarto890@gmail.com>
2025-06-29 14:33:56 +0700

    Potonga Clear dan Logo

--- internal/domain/models/ak_izin.go
+++ internal/domain/models/ak_izin.go
@@ -5,7 +5,7 @@ import "time"
 type Ak_Izin struct {
 	IzinId      int64     `gorm:"primaryKey" json:"izin_id"`
 	UserUID     string    `gorm:"type:varchar(30)" json:"user_uid"`
-	IzinType    string    `gorm:"type:varchar(20)" json:"izin_tipe"`
+	IzinType    string    `gorm:"type:varchar(20)" json:"izin_type"`
 	StartDate   string    `gorm:"type:date" json:"start_date"`
 	EndDate     string    `gorm:"type:date" json:"end_date"`
 	Alasan      string    `gorm:"type:text" json:"alasan"`
