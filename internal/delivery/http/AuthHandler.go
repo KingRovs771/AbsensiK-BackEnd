b56cb8127036ed11af6c20daa784ee5f4a6a3df2
rzbudi__ <rizkybudiarto890@gmail.com>
2025-06-29 21:17:42 +0700

    Done Izin

--- internal/delivery/http/AuthHandler.go
+++ internal/delivery/http/AuthHandler.go
@@ -89,6 +89,10 @@ func (h *AuthHandler) GetUserInfo(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	json.NewEncoder(w).Encode(user)
 }
 
