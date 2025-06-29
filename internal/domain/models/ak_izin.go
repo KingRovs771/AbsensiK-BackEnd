9fede7aa78662dfcd9cc96ea8236d2d9d950bcc3
rzbudi__ <rizkybudiarto890@gmail.com>
2025-06-29 15:37:44 +0700

    Potonga Clear dan Logo

--- internal/domain/models/ak_izin.go
+++ internal/domain/models/ak_izin.go
@@ -1,16 +1,16 @@
 package models
 
 type Ak_Izin struct {
-	IzinId      int64  `gorm:"primaryKey" json:"izin_id"`
-	UserUID     string `gorm:"type:varchar(30)" json:"user_uid"`
-	IzinType    string `gorm:"type:varchar(20)" json:"izin_type"`
-	StartDate   string `gorm:"type:date" json:"start_date"`
-	EndDate     string `gorm:"type:date" json:"end_date"`
-	Alasan      string `gorm:"type:text" json:"alasan"`
-	Status      int    `gorm:"type:int" json:"status"`
-	Foto        []byte `gorm:"type:longblob;" json:"foto"`
-	ApproveBy   string `gorm:"type:varchar(30)" json:"approve_by"`
-	ApproveDate string `gorm:"type:date" json:"approve_date"`
+	IzinId      int64   `gorm:"primaryKey" json:"izin_id"`
+	UserUID     string  `gorm:"type:varchar(30)" json:"user_uid"`
+	IzinType    string  `gorm:"type:varchar(20)" json:"izin_type"`
+	StartDate   string  `gorm:"type:date" json:"start_date"`
+	EndDate     string  `gorm:"type:date" json:"end_date"`
+	Alasan      string  `gorm:"type:text" json:"alasan"`
+	Status      int     `gorm:"type:int" json:"status"`
+	Foto        *[]byte `gorm:"type:longblob;" json:"foto"`
+	ApproveBy   *string `gorm:"type:varchar(30)" json:"approve_by"`
+	ApproveDate *string `gorm:"type:date" json:"approve_date"`
 
 	Users Ak_Users `gorm:"foreignKey:UserUID;references:UserUID" json:"users"`
 }
