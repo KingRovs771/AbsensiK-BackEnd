5a0812c8b75258e2945c231752484731a3b85d11
rzbudi__ <rizkybudiarto890@gmail.com>
2025-07-03 20:32:33 +0700

    Done Izin

--- internal/domain/repository/izin_repository.go
+++ internal/domain/repository/izin_repository.go
@@ -69,9 +69,9 @@ func (r *IzinRepository) ApproveIzin(IzinId int64, ApproveBy string) error {
 func (r *IzinRepository) GetPermitsByUserUID(userUID string) ([]models.Ak_Izin, error) {
 	var permits []models.Ak_Izin
 
-	// Mencari semua data di tabel 'ak_izins' yang cocok dengan user_uid.
-	// Kita juga mengurutkannya dari yang terbaru (berdasarkan timestamp).
-	err := r.DB.Where("user_uid = ?", userUID).Order("created_at desc").Find(&permits).Error
+	err := r.DB.Preload("Users", func(db *gorm.DB) *gorm.DB {
+		return db.Select("user_uid, full_name")
+	}).Where("user_uid = ?", userUID).Order("created_at desc").Find(&permits).Error
 	if err != nil {
 		return nil, err
 	}
